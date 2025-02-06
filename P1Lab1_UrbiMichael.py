6efbc0489ce0e2faa14a21e3cab62ce12008fe1c
Griffen82 <urbim7332@student.faytechcc.edu>
2025-02-06 14:21:41 -0500

    Add files via upload

---
+++ P1Lab1_UrbiMichael.py
@@ -0,0 +1,14 @@
+#Michael Urbi
+#Febuary 6, 2025
+#P1Lab1
+#Lab work introducing Python
+
+#Input information
+
+fname = input("Enter your first name: ")
+
+lname = input("Enter your last name: ")
+
+#Welcome message
+
+print("Hello, " + str(fname)+ " " + str(lname)+ "! Welcome to CTI-110")
