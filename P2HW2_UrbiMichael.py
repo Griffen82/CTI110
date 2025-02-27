93622b7d26fa5df193976ef747bfba0aaaac0d64
Griffen82 <urbim7332@student.faytechcc.edu>
2025-02-27 14:33:17 -0500

    Add files via upload

---
+++ P2HW2_UrbiMichael.py
@@ -0,0 +1,31 @@
+#Michael Urbi
+#Febuary 27, 2025
+#P2HW2
+#Program showing comprehension of list elements
+
+#Ask user to input grades from Modules 1-6
+mod1 = float(input("Enter grade for Module 1: "))
+mod2 = float(input("Enter grade for Module 2: "))
+mod3 = float(input("Enter grade for Module 3: "))
+mod4 = float(input("Enter grade for Module 4: "))
+mod5 = float(input("Enter grade for Module 5: "))
+mod6 = float(input("Enter grade for Module 6: "))
+
+#Store the information in a list
+grades = list['mod1', 'mod2', 'mod3', 'mod4', 'mod5', 'mod6']
+
+#Display common inquiries of grades and sum of grades
+print()
+print("-"*12, "Results", "-"*12)
+print(grades)
+#print(f"{"Lowest Grade:":<20} {}")
+
+
+#print(f"{"Highest Grade:":<20} {}")
+
+
+#print(f"{"Sum of Grades:":<20} {}")
+
+
+#print(f"{"Average:":<20} {}")
+print("-"*40)
